b3ad65b8143272e496ed996592eaf97114876919
Brian Almada <brianalexisalmada84@gmail.com>
2024-06-12 23:01:58 -0300

    Configurar los middlewares de cors y agrandando los schemas

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT
+const cors = require('cors');
 const Boock = require('./models/books');
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+})
+
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Boock.find();
+        res.status(200).json({ok: true, data: books});
+    } catch (error) {
+        res.status(400).json({ok: false, error});
+    }
 })
 
 app.post('/books', async (req, res) => {
@@ -21,13 +32,13 @@ app.post('/books', async (req, res) => {
     }
 })
 
-    mongoose
-        .connect(process.env.MONGO_URL, {})
-            .then((conn) => {
-                app.listen(PORT, async() => {
-                    console.log(`App escuchando en PORT ${PORT}`);
-                })
+mongoose
+    .connect(process.env.MONGO_URL, {})
+        .then((conn) => {
+            app.listen(PORT, async() => {
+                console.log(`App escuchando en PORT ${PORT}`);
             })
-            .catch((err) => console.log({err}))
+        })
+        .catch((err) => console.log({err}))
 
 
